c2ec7864a54c188ffc6472889b73cce7a1f8bca7
Prashant Gupta <146712970+Prashant04627@users.noreply.github.com>
2024-05-15 00:40:12 +0530

    Delete src directory

--- src/store/Calender.js
+++
@@ -1,9 +0,0 @@
-import { create } from "zustand";
-import { groupNumber, INITIAL_EVENTS, userData, boardData } from "../data/data";
-
-const useCalender = create((set) => ({
-  currentEvents: INITIAL_EVENTS,
-  setCurrentEvents: (events) => set({ currentEvents: events }),
-}));
-
-export default useCalender;
